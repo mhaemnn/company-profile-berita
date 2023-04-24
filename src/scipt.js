7f5baa9a5d94ad41a2022fb54acedf610064bc13
mhaemnn <ismuhaimin1@gmail.com>
2023-04-24 19:15:13 +0700

    solved javascript issue for slider and navbar🚀

--- src/scipt.js
+++ src/scipt.js
@@ -2,22 +2,6 @@
 let btnMenu = document.getElementById("btn-menu");
 let menu = document.getElementById("menu");
 
-btnMenu.onclick = () => {
-  btnMenu.classList.toggle("fa-times");
-  menu.classList.toggle("active");
-};
-toggleSwitch.addEventListener("change", switchTheme, false);
-
-// addEvenLisner
-function toggleForm() {
-  var form = document.getElementById("form");
-  if (form.style.display === "none") {
-    form.style.display = "block";
-  } else {
-    form.style.display = "none";
-  }
-}
-
 // banner / slideshow
 const slider = document.querySelector(".slider");
 const nextBtn = document.querySelector(".next-btn");
@@ -98,3 +82,19 @@ slider.addEventListener("mouseover", () => {
 slider.addEventListener("mouseout", () => {
   repeater();
 });
+
+btnMenu.onclick = () => {
+  btnMenu.classList.toggle("fa-times");
+  menu.classList.toggle("active");
+};
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+// addEvenLisner
+function toggleForm() {
+  var form = document.getElementById("form");
+  if (form.style.display === "none") {
+    form.style.display = "block";
+  } else {
+    form.style.display = "none";
+  }
+}
