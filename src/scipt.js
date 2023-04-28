9a54a44727474c5d3d6cc8a8ff6ef0a3fd552b48
kinchanie <nimadecitrarianisari@gmail.com>
2023-04-28 10:28:32 +0800

    add logo slider

--- src/scipt.js
+++ src/scipt.js
@@ -136,3 +136,38 @@ function closePopup(popup) {
   popup.classList.remove('active')
   overlay.classList.remove('active')
 }
+
+// logo slider
+$('.logo-slider').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  responsive: [{
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false
+          }
+      },
+      {
+          breakpoint: 600,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+          }
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+          }
+      }
+  ]
+});
