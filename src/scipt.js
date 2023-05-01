daee4b1c94bdd8c2a33c313025ec3fa3621693c3
mhaemnn <ismuhaimin1@gmail.com>
2023-05-01 22:44:43 +0700

    rename title in html file

--- src/scipt.js
+++ src/scipt.js
@@ -99,44 +99,6 @@ function toggleForm() {
   }
 }
 
-// popup screen
-const openPopupButtons = document.querySelectorAll("[data-popup-target]");
-const closeButtons = document.querySelectorAll("[data-close-button]");
-const overlay = document.getElementById("overlay");
-
-openPopupButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const popup = document.querySelector(button.dataset.popupTarget);
-    openPopup(popup);
-  });
-});
-
-overlay.addEventListener("click", () => {
-  const popup = document.querySelectorAll(".popup.active");
-  popup.forEach((popup) => {
-    closePopup(popup);
-  });
-});
-
-closeButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const popup = button.closest(".popup");
-    closePopup(popup);
-  });
-});
-
-function openPopup(popup) {
-  if (popup == null) return;
-  popup.classList.add("active");
-  overlay.classList.add("active");
-}
-
-function closePopup(popup) {
-  if (popup == null) return;
-  popup.classList.remove("active");
-  overlay.classList.remove("active");
-}
-
 // logo slider
 $(".logo-slider").slick({
   slidesToShow: 5,
